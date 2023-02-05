ce2df51fcf1b3449cf173791fd56eed3ee6a9294
choi <choinamename@gmail.com>
2023-02-05 21:22:17 +0900

    [feat][fix] 상품 바 css,html 생성 및 수정

--- client/js/basket/basket.js
+++ client/js/basket/basket.js
@@ -1,6 +1,1 @@
-// import { getNode } from '../../lib/index'
-
-
-console.log("브랜치 스위칭을 위한 테스트 문구입니다. 지워도 됩니다.");
-console.log("브랜치 스위칭을 위한 테스트 문구입니다. 지워도 됩니다.");
-console.log("브랜치 스위칭을 위한 테스트 문구입니다. 지워도 됩니다.");
+import { getNode } from '../../lib/index.js'
