d1875cf76b43ef94d58ecb6f68f5a84403fc00ab
djswns7 <duftlagltkfwk@gmail.com>
2023-02-06 22:48:39 +0900

    [fix] index.html 수정

--- client/js/main-page/create-new-product-card.js
+++ client/js/main-page/create-new-product-card.js
@@ -1,12 +1,24 @@
+const SERVER_URL = 'http://localhost:3000/';
+
+let productArr;
+
+fetch(`${SERVER_URL}products`)
+  .then(res => res.json())
+  .then(data =>{
+    productArr = data;
+    console.log(productArr);
+  })
 
 
 // document.createElement('생성하고자 하는 태그명')
-let newdiv = document.createElement('div');
+// let newdiv = document.createElement('div');
 const body = document.querySelector('body');
 // body.appendChild(newdiv);
 // body.insertAdjacentHTML(position, text);
 //text(인자)는 HTML 또는 XML로 해석될 수 있는 문자열이고(html code), (DOM) tree에 삽입할 수 있다.
-body.insertAdjacentElement('afterbegin',newdiv);
+// body.insertAdjacentElement('afterbegin',newdiv);
+
+
 
 Object.assign(
   document.createElement('div'),
@@ -14,53 +26,82 @@ Object.assign(
   { textContent: 'Hello' }
 )
 
-let mainPageCard = /*html*/
-`<a class="product-display_product-card" href="./product_detail.html">
-<figure>
-  <img class="product-display_product-img" src="./assets/productcard/01pumuone.jpg" alt="탱탱쫄면 (4개입)" />
-  <figcaption class="a11y-hidden">탱탱쫄면 4개입</figcaption>
-  <div class="product-display_cart-icon-container">
-    <img class="product-display_cart-icon" src="./assets/icons/web-icons/Cart.svg" alt="장바구니 아이콘" />
-  </div>
-</figure>
-<div class="product-display_info-wrapper">
-  <mark class="product-display_shipping-info">샛별배송</mark>
-  <mark class="product-display_product-name">[풀무원] 탱탱쫄면 (4개입)</mark>
+//option 1 이면 메인페이지, 2면 product-list
+function makeCard(option,netxtPageLink,imageUrl,productName,saleRatio,salePrice,price,description,isKarlyOnly,isLimitedProduct) {
+  let newCard = /*html*/
+  `
+<li>
+  <a class="product-display_product-card" href="${netxtPageLink}">
+    <figure>
+      <img class="product-display_product-img" src="./assets/productcard/${imageUrl}.jpg" alt="${productName}" />
+      <figcaption class="a11y-hidden">${productName}</figcaption>
+    </figure>
+    <div class="product-display_cart-icon-container">
+      <img class="product-display_cart-icon" src="./assets/icons/web-icons/Cart.svg" alt="장바구니 아이콘" />
+    </div>
+    <div class="product-display_info-wrapper">
+      <mark class="product-display_shipping-info">
+        
+      </mark>
+      <mark class="product-display_product-name">
+        ${productName} 
+      </mark>
+      <div class="product-display_price-wrapper">
+        <mark class="product-display_discount-rate">${saleRatio}</mark>
+        <mark class="product-display_sell-price">${salePrice}</mark>
+      </div>
+      <div class="product-display_undiscounted-price">${price}</div>
+      <div class="product-display_product-detail">
+        ${description}
+      </div>
+    </div>
+  </a>
+</li>
+  `;
+  if(option == 2){
+    newCard = /*html*/
+    `
+      <a class="product-display_product-card" href="${netxtPageLink}">
+        <figure>
+          <img class="product-display_product-img" src="./assets/productcard/${imageUrl}.jpg" alt="${productName}" />
+          <figcaption class="a11y-hidden">${productName}</figcaption>
+        </figure>
+        <div class="product-display_cart-icon-container">
+          <img class="product-display_cart-icon" src="./assets/icons/web-icons/Cart.svg" alt="장바구니 아이콘" />
+        </div>
+        <div class="product-display_info-wrapper">
+          <mark class="product-display_shipping-info">
+            
+          </mark>
+          <mark class="product-display_product-name">
+            ${productName} 
+          </mark>
+          <div class="product-display_price-wrapper">
+            <mark class="product-display_discount-rate">${saleRatio}</mark>
+            <mark class="product-display_sell-price">${salePrice}</mark>
+          </div>
+          <div class="product-display_undiscounted-price">${price}</div>
+          <div class="product-display_product-detail">
+            ${description}
+          </div>
+          <!--<div class="product-display_badge-wrapper">
+            <mark class="product-display_karly-only"></mark>
+            <mark class="product-display_limited-products"></mark>
+          </div>-->
+        </div>
+      </a>
+      `
+  }else if(isKarlyOnly){
+    newCard = '';
+  }else if(isLimitedProduct){
+    newCard = '';    
+  }
 
-  <div class="product-display_price-wrapper">
-    <mark class="product-display_discount-rate">50%</mark>
-    <mark class="product-display_sell-price">5,000원</mark>
-  </div>
+  return newCard;
+}
 
-  <div class="product-display_undiscounted-price">10,000원</div>
-  <div class="product-display_product-detail">CJ즉석밥 고소한 맛의 발아 현미밥</div>
+function putCard(selector,card){
+  document.querySelector(selector).insertAdjacentHTML("beforeend",card)
+}
 
-  <div class="product-display_badge-wrapper">
-    <mark class="product-display_karly-only"></mark>
-    <mark class="product-display_limited-products"></mark>
-  </div>
-</div>
-</a>
-`
-
-// let productListCard = /*html*/
-// `
-// <figure class="card">
-//   <a href="#">
-//     <img src="./assets/productcard/01pulmuone.jpg" alt="">
-//     <figcaption class="content">
-//       <span class="delivery">샛별배송</span>
-//       <span class="product-name">[풀무원] 탱탱쫄면 (4개입)</span>
-//       <span class="product-price">4,980 원</span>
-//       <p class="description">튀기지 않아 부담없는 매콤함</p>
-//       <div class="tag">
-//         <span class="only">Karly Only</span>
-//         <span class="limited">한정수량</span>
-//       </div>
-//     </figcaption>
-//   </a>
-//   <div class="list-cart">
-//     <img src="./assets/icons/web-icons/Cart.svg" alt="" />
-//   </div>
-// </figure>
-// `
+putCard('.product-display_product-cards-container',makeCard(1,'./product_detail.html','01pumuone','쫄면입니다','50%','5000','10000','즉석밥이랍니다'));
