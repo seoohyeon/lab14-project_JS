7434450fc82a0f4df5e00be6508e5dfad6fe13f7
DahamWeon <onedaham@naver.com>
2023-02-07 13:16:15 +0900

    [setting] 세팅 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -131,5 +131,8 @@ module.exports = {
     'prefer-template': ['warn'],
     // 객체의 프로퍼티 이름은 필요할 때만 따옴표로 감싸야 한다
     'quote-props': ['warn', 'as-needed'],
+
+    'no-undef':'warn',
+    'no-unused-vars':'warn'
   },
 }
