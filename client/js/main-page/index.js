87fb0fff69357564ad97e1b51b8ab9db4a53b864
djswns7 <duftlagltkfwk@gmail.com>
2023-02-08 21:36:11 +0900

    [feat] main-page 장바구니 팝업기능 (localStorage 연동 X)

--- client/js/main-page/index.js
+++ client/js/main-page/index.js
@@ -1,6 +1,7 @@
 export * from './recent-product.js'
 export * from './create-new-product-card.js'
 export * from './todays-Ad-popup.js'
+export * from './main-page.js'
 export * from './swiper-style.js'
 export * from '../global.js'
 
---
+++ client/js/main-page/main-page.js
@@ -0,0 +1,87 @@
+import { changePriceToString,changeStringToPrice } from "../../lib/index.js";
+
+
+
+const regex = /[^0-9]/g;
+let productDisplaySwiper1 = document.querySelector('.product-display_swiper1');
+let productDisplaySwiper2 = document.querySelector('.product-display_swiper2');
+
+let productPrice = 0;
+let productCount =1;
+let totalPrice = productCount * productPrice;
+
+let cartPopupWrapper = document.querySelector('.cart-popup');
+let cartPopupTotalPrice = document.querySelector('.cart-popup_price');
+let cartPopupTotalCount = document.querySelector('.cart-popup_count-total');
+let cartPopupSinglePrice = document.querySelector('.cart-popup_content-count span');
+let cartPopupTitle = document.querySelector('.cart-popup_content-title span');
+
+let cartPopupCancelBtn = document.querySelector('.cart-popup_cancel-button');
+let cartPopupAddBtn = document.querySelector('.cart-popup_add-button');
+let cartPopupPlusBtn = document.querySelector('.cart-popup_count-plus');
+let cartPopupMinusBtn = document.querySelector('.cart-popup_count-minus');
+
+productDisplaySwiper1.addEventListener('click',(e)=>{
+  e.preventDefault();
+  let curSlide =  e.target.closest('li');
+  if(e.target.classList.contains('product-display_cart-icon')){
+    productPrice = curSlide.querySelector('.product-display_sell-price').textContent;
+    productPrice = changeStringToPrice(productPrice);
+    
+    cartPopupTotalPrice.textContent = changePriceToString(productPrice);
+    cartPopupTotalCount.textContent = 1
+    cartPopupSinglePrice.textContent = changePriceToString(productPrice);
+    cartPopupTitle.textContent = curSlide.querySelector('.product-display_product-name').textContent;
+  }
+})
+
+productDisplaySwiper2.addEventListener('click',(e)=>{
+  e.preventDefault();
+  let curSlide = e.target.closest('li');
+  if(e.target.classList.contains('product-display_cart-icon')){
+    productPrice = curSlide.querySelector('.product-display_sell-price').textContent;
+    productPrice = changeStringToPrice(productPrice);
+    
+
+  }
+})
+
+
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                                 팝업에서의 프로세스                          */
+/* -------------------------------------------------------------------------- */
+
+
+
+cartPopupWrapper.addEventListener('click',(e)=>{
+  e.preventDefault();
+  let curBtn = e.target.closest('btn');
+  totalPrice = productCount * productPrice;
+  if(e.target.classList.contains('cart-popup_cancel-button')){
+    // 꺼지는 이벤트가 이미 있음
+    // console.log('closed');
+  }else if(e.target.classList.contains('cart-popup_add-button')){
+    // 꺼지는 이벤트가 이미 있음
+    console.log('상품을 장바구니에 담아야함');
+  }else if(e.target.classList.contains('cart-popup_count-plus')){
+    productCount++;
+    totalPrice = productCount * productPrice;
+    e.currentTarget.querySelector('.cart-popup_count-total').textContent = productCount;
+    e.currentTarget.querySelector('.cart-popup_price').textContent = changePriceToString(totalPrice);
+
+  }else if(e.target.classList.contains('cart-popup_count-minus')){
+    productCount--;
+    totalPrice = productCount * productPrice;
+    e.currentTarget.querySelector('.cart-popup_count-total').textContent = productCount;
+    e.currentTarget.querySelector('.cart-popup_price').textContent = changePriceToString(totalPrice);
+    
+
+  }else{
+    // 버튼이 아닌곳을 눌렀을 때
+  }
+
+
+})
