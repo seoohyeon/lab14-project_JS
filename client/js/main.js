0d64c817a05a9dd49fd6cdd70dd48001d2f50db9
seohyeon <fltk0825@naver.com>
2023-02-09 00:33:57 +0900

    [fix] basket페이지 로그아웃 구현

--- client/js/main.js
+++ client/js/main.js
@@ -2,3 +2,4 @@ import * as main from "./main-page/index.js";
 import * as PRODCUT_DETAIL from './product_detail/index.js' 
 import * as SIGNUP from './signup/index.js'
 import * as LOGIN from './login/index.js' 
+import * as BASKET from './basket/index.js' 
